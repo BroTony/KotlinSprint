5c3841a25fd64728234bef9285a4c8a0eb83a467
Антон <test@test.ru>
2023-05-07 20:00:14 +0300

    KS-1-5

---
+++ src/main/kotlin/1-5.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    val seconds = 6480
+    var minute = seconds / 60
+    var ostatokSeconds = seconds % 60
+
+    print(minute)
+    print(":")
+    if (ostatokSeconds < 10) {
+        print("0")
+        print(ostatokSeconds)
+    }
+    else
+        println(ostatokSeconds)
+
+
+
+}
