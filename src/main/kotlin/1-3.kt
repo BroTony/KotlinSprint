1688ce08d617e65ad0d8e49d5a88e87111b6ac4a
Антон <test@test.ru>
2023-05-07 19:14:52 +0300

    KS-1-2

---
+++ src/main/kotlin/1-3.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    val year = 1961
+    var hour: Byte = 9
+    var minute: Byte = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+
+
+
+
+
+}
