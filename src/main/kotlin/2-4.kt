334934a7a801120e2af6d1dbef84d6dfcc243a7a
Антон <test@test.ru>
2023-05-08 16:14:34 +0300

    KS-2-4

---
+++ src/main/kotlin/2-4.kt
@@ -0,0 +1,8 @@
+fun main(){
+
+    val kristalCount = 7
+    val ferCount = 11
+    println("Количество кристаллов стало " + kristalCount * 6 / 5)
+    println("Количество железной руды стало " + ferCount * 6 / 5)
+
+}
