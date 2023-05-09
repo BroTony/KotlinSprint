31cd61adc57a6505b75ed3248442cb3fda07961d
Антон <test@test.ru>
2023-05-09 10:17:54 +0300

    KS-3-4

---
+++ src/main/kotlin/3-4.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    var from = "E2"
+    var to = "E4"
+    var num = 1
+    var  stroca = "$from, $to, $num"
+    println(stroca)
+    from = "D2"
+    to = "D4"
+    num++
+    stroca = "$from, $to, $num"
+    println(stroca)
+
+}
