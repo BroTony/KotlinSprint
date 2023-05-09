cad6a59f856fbc72cea1f970501597773ae797b7
Антон <test@test.ru>
2023-05-09 09:36:50 +0300

    KS-3-2

---
+++ src/main/kotlin/3-2.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    var fioBefore = "Андреева Татьяна Сергеевна"
+    var fioAfter = "Сидорова Татьяна Сергеевна"
+    var ageBefore = 20
+    var ageAfter = 22
+
+    println("$fioBefore, $ageBefore")
+    println("$fioAfter, $ageAfter")
+
+}
