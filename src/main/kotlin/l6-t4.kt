29c5a821f93582c1e4e94e858b3eaa74b27be1cc
Антон <test@test.ru>
2023-05-10 19:50:18 +0300

    KS-6-4

---
+++ src/main/kotlin/l6-t4.kt
@@ -0,0 +1,18 @@
+fun main (){
+
+    val numbWin = (1..9).random()
+    var count = 0
+    var numb:Int
+    do {
+        println("Введите число от 1 до 9")
+        numb = readln()!!.toInt()
+        if (numb == numbWin)
+            println("Это была великолепная игра")
+        count++
+    } while (numb != numbWin && count < 5)
+    if (numb != numbWin && count==5)
+    println("Было загадано число $numbWin")
+
+
+
+}
