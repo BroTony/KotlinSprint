47db904220486c76fe08c0ec6b2bbf843c0b2e52
Антон <test@test.ru>
2023-05-10 13:35:14 +0300

    KS-5-1

---
+++ src/main/kotlin/l5-t1.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    println("htibnt ghbvth 6+5")
+    var num = readln()!!.toInt()
+    if (num == 11)
+        println("Добро пожаловать!")
+    else
+        println("Доступ запрещен")
+
+
+
+}
