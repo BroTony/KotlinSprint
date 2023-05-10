ae79372427cc544894e49d71982e149c4adadded
BroTony <132762023+BroTony@users.noreply.github.com>
2023-05-10 20:10:43 +0300

    Update l6-t1.kt
    вроде так лучше

--- src/main/kotlin/l6-t1.kt
+++ src/main/kotlin/l6-t1.kt
@@ -4,11 +4,12 @@ fun main(){
     val log = readln()
     println("придумайте пароль")
     val password = readln()
+
     do {
         println("Enter password")
-        var psw = readln()
-        if (psw == password)
+        var passwordRepeat = readln()
+        if (passwordRepeat == password)
             println("Welcome")
-    } while (psw != password)
+    } while (passwordRepeat != password)
 
 }
