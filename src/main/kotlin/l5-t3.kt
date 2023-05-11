7dc5b91b91c75e974df6c07182b0c81cf0a7b95a
Антон <test@test.ru>
2023-05-11 13:14:18 +0300

    KS-5-3

--- src/main/kotlin/l5-t3.kt
+++ src/main/kotlin/l5-t3.kt
@@ -2,10 +2,12 @@ fun main(){
 
     val numTrue1 = 69
     val numTrue2 = 11
+
     println("введите число 1")
-    val num1 = readln()!!.toInt()
+    val num1 = readln().toInt()
     println("введите число 2")
-    val num2 = readln()!!.toInt()
+    val num2 = readln().toInt()
+
     if (num1 != numTrue1 && num1 != numTrue2 && num2 != numTrue1 && num1 != numTrue2)
         println("Неудача! Крутите барабан")
     else if ((num1 == numTrue1 && num2 == numTrue2) || (num2 == numTrue1 && num1 == numTrue2))
