76704d317864ff01a0d82f191ac942b11cf12227
Антон <test@test.ru>
2023-05-11 13:24:41 +0300

    KS-5-4

--- src/main/kotlin/l5-t4.kt
+++ src/main/kotlin/l5-t4.kt
@@ -1,11 +1,12 @@
 fun main(){
 
     println("Введите логин")
-    val log = readln()
-    if (log == "Batman"){
+    val login = readln()
+
+    if (login == LOGIN_BATMAN){
         println("Введите пароль, Брюс Уэйн")
         val password = readln()
-        if (password == "password")
+        if (password == PASSWORD_HERO)
             println("Это Вы, Брюс, система узнала Вас по криптостойкому паролю, никто бы его не угадал")
         else
             println("До Бэтмена не дотягиваешь, мышонок")
@@ -15,3 +16,5 @@ fun main(){
         println("Может зарегистрируйтесь, у нас тут есть Бэтмен с паролем, который не угадаете")
 
 }
+const val LOGIN_BATMAN = "Batman"
+const val PASSWORD_HERO = "password"
