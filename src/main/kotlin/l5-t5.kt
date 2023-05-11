4f59382917333de88e8c73f14cb419e7186084ea
Антон <test@test.ru>
2023-05-11 13:31:40 +0300

    KS-5-5

--- src/main/kotlin/l5-t5.kt
+++ src/main/kotlin/l5-t5.kt
@@ -2,10 +2,12 @@ fun main(){
 
     val numTrue1 = (1..100).random()
     val numTrue2 = (1..100).random()
+
     println("введите число 1")
-    val num1 = readln()!!.toInt()
+    val num1 = readln().toInt()
     println("введите число 2")
-    val num2 = readln()!!.toInt()
+    val num2 = readln().toInt()
+
     if (num1 != numTrue1 && num1 != numTrue2 && num2 != numTrue1 && num1 != numTrue2)
         println("Неудача! Крутите барабан, числа были $numTrue1 и $numTrue2")
     else if ((num1 == numTrue1 && num2 == numTrue2) || (num2 == numTrue1 && num1 == numTrue2))
