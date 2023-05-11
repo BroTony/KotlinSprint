27b1c0ad713186f82d2f2dcdc54204a1a59658f8
Антон <test@test.ru>
2023-05-11 19:01:50 +0300

    KS-6-4

--- src/main/kotlin/l6-t4.kt
+++ src/main/kotlin/l6-t4.kt
@@ -6,7 +6,7 @@ fun main (){
 
     do {
         println("Введите число от 1 до 9")
-        numb = readln()!!.toInt()
+        numb = readln().toInt()
         if (numb == numbWin)
             println("Это была великолепная игра")
         count++
