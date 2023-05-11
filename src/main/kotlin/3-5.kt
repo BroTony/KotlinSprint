b8220723d24e738cccab5fd38637be7aa8384771
Антон <test@test.ru>
2023-05-11 12:41:59 +0300

    KS-3-5

--- src/main/kotlin/3-5.kt
+++ src/main/kotlin/3-5.kt
@@ -1,9 +1,9 @@
 fun main(){
 
-    val stroca = "D2-D4:0"
-    val from = stroca.substringBefore('-')
-    val to = stroca.substringAfter('-').substringBefore(':')
-    val num = stroca.substringAfter(':')
+    val inFile = "D2-D4:0"
+    val from = inFile.substringBefore('-')
+    val to = inFile.substringAfter('-').substringBefore(':')
+    val num = inFile.substringAfter(':')
     println("$from  $to  $num")
 
 }
