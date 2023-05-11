b1071d01c28fb01e55c3386c1ce278089ead8e45
Антон <test@test.ru>
2023-05-11 12:55:20 +0300

    KS-5-1

--- src/main/kotlin/l5-t1.kt
+++ src/main/kotlin/l5-t1.kt
@@ -1,12 +1,11 @@
 fun main(){
 
-    println("htibnt ghbvth 6+5")
+    println("напишите правильный ответ 6 + 5")
     var num = readln()!!.toInt()
+
     if (num == 11)
         println("Добро пожаловать!")
     else
         println("Доступ запрещен")
-
-
 
 }
