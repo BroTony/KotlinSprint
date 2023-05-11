1655615b39b2c242752dc0a8a199892a239bb908
Антон <test@test.ru>
2023-05-11 13:06:42 +0300

    KS-5-2

--- src/main/kotlin/l5-t2.kt
+++ src/main/kotlin/l5-t2.kt
@@ -2,7 +2,7 @@ fun main(){
 
     println("Введите год рождения")
     val yearNow = 2023
-    val yearBorn = readln()!!.toInt()
+    val yearBorn = readln().toInt()
     if ((yearNow - yearBorn) < AGE_OF_MAGORITY)
        println("Доступ закрыт")
     else
