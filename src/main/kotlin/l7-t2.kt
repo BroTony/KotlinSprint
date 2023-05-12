61a1ea0d6d91f24a097965715bb0492fa13b5fd6
Антон <test@test.ru>
2023-05-12 16:41:32 +0300

    KS-7-2

--- src/main/kotlin/l7-t2.kt
+++ src/main/kotlin/l7-t2.kt
@@ -3,12 +3,15 @@ fun main() {
     var generatePassword: Int
     var testPassword: Int
 
-    do {
+    while (true) {
         generatePassword = (1000..9999).random()
         println("Ваш код авторизации: $generatePassword")
         testPassword = readln().toInt()
-        if (generatePassword == testPassword)
+        if (generatePassword == testPassword) {
             println("Приветствую, человек")
-    } while (generatePassword != testPassword)
+            break
+        }
+    }
+       // while (generatePassword != testPassword)
 
 }
