d655303f687b78e04585d3463ed2b58c9bf024e9
Антон <test@test.ru>
2023-05-12 16:22:22 +0300

    KS-7-3

---
+++ src/main/kotlin/l7-t3.kt
@@ -0,0 +1,8 @@
+fun main(){
+
+    println("Введите число, до которого вывести все чётные числа")
+    val numberFinal = readln().toInt()
+    for (i in 2..numberFinal step 2)
+            println("$i")
+
+}
