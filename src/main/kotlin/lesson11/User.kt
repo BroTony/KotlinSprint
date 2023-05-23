22d11f759a5a69b22cfcbf1893fc7873472bc23b
Антон <test@test.ru>
2023-05-23 16:25:54 +0300

    KS-11-1

---
+++ src/main/kotlin/lesson11/User.kt
@@ -0,0 +1,17 @@
+package lesson11
+
+class User (
+    val id: Int,
+    val log: String,
+    val password: String,
+    val email: String,
+){
+
+    fun printAllDates(){
+        println(id)
+        println(log)
+        println(password)
+        println(email)
+    }
+
+}
---
+++ src/main/kotlin/lesson11/lesson11.kt
@@ -0,0 +1,21 @@
+package lesson11
+
+fun main(){
+
+    val user1 = User(
+        id = 0,
+        log = "BroAnton",
+        password = "12345",
+        email = "anton@mail.com",
+    )
+    val user2 = User(
+        id = 1,
+        log = "BroIvan",
+        password = "123456",
+        email = "Ivan@mail.com",
+    )
+
+    user1.printAllDates()
+    user2.printAllDates()
+
+}
