6fef07d9f857cb21aa28940b8d71ff148c6f0b15
Антон <test@test.ru>
2023-06-03 21:19:31 +0300

    KS-12-1

--- src/main/kotlin/l12-t1.kt
+++ src/main/kotlin/l12-t1.kt
@@ -3,22 +3,22 @@ fun main(){
     val goodDay = Weathter()
         goodDay.tempDay = 30
         goodDay.tempNight = 20
-        goodDay.rain = false
+        goodDay.isRain = false
     val badDay = Weathter()
         badDay.tempDay = 10
         badDay.tempNight = 0
-        badDay.rain = true
+        badDay.isRain = true
 
 }
 
-class Weathter() {
+class Weathter {
     var tempDay = 20
     var tempNight = 10
-    var rain = false
+    var isRain = false
 
     init {
         println("температура днём $tempDay")
         println("температура ночью $tempNight")
-        println("наличие дождя $rain")
+        println("наличие дождя $isRain")
     }
 }
